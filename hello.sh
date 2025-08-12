7b7fd309c84324eb8e5076ff282379c3df2436fd
mehedi <mehedi15-2719@diu.edu.bd>
2025-08-12 06:39:09 +0000

    Updated hello.sh with latest changes

--- hello.sh
+++ hello.sh
@@ -1,4 +1,8 @@
 echo '#!/bin/bash' > hello.sh
 echo 'echo "Hello from Ubuntu VM!"' >> hello.sh
 chmod +x hello.sh
+echo '#!/bin/bash' > hello.sh
+echo 'echo "Hello from Ubuntu VM!"' >> hello.sh
+chmod +x hello.sh
+
 
